7387e0ec0120942ecb909376a34b37f58dac44dd
Milos Curuvija <curuvija@live.com>
2024-05-24 16:42:56 +0200

    Feature/add GitHub actions (#5)
    * Release version 2.1.0

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        "@commitlint/config-conventional"
+    ],
+    rules: {
+        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "refactor", "revert", "style", "test"]],
+        "scope-enum": [2, "always", ["chart", "test"]]
+    }
+}
